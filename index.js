5203403d8a37e1c64adaec21a4fe404b9fd60db3
jadsonoc <jrjedi@gmail.com>
2022-08-29 23:35:13 -0300

    Resolucao dos exercicio

--- index.js
+++ index.js
@@ -12,8 +12,15 @@ function invert(people){
     // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
     // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
     // nomes dos usuários.
-
-    return [];
+    let arrNames = ( Array.isArray(people) ) ? people : [];
+    /*if ( people.length === 0 ) {
+        alert("Por favor, insira 3 nomes!");
+        arrNames.push( prompt("Digite o primeiro nome: ") );
+        arrNames.push( prompt("Digite o segundo nome: ") );
+        arrNames.push( prompt("Digite o terceiro nome: ") );
+    }*/
+	arrNames.reverse();
+    return arrNames;
 }
 
 /**
@@ -28,8 +35,24 @@ function mean(grades){
     // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
     // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
     // ou superior a 7.
-
-    return 0;
+    let arrGrades = ( Array.isArray(grades) ) ? grades : [];
+    let fltMean = 0.0;
+    let intGrades = 0;
+    /*
+    let strName = "O Aluno";
+    if ( arrGrades.length === 0 ) {
+        strName = prompt( "Informe o nome de um aluno: " );
+        arrGrades.push( prompt("Digite a primeira nota: ") );
+        arrGrades.push( prompt("Digite a segunda nota: ") );
+        arrGrades.push( prompt("Digite a terceira nota: ") );
+    }*/
+    intGrades = arrGrades.length;
+    for ( let i = 0; i < intGrades; i++ ) {
+        fltMean += arrGrades[i];
+    }
+    fltMean /= intGrades;
+    //return strName + " foi " + isApproved( fltMean ) + " com a média: " + parseFloat( fltMean );
+    return fltMean;
 }
 
 /**
@@ -44,8 +67,7 @@ function isApproved(mean){
     // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
     // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
     // ou superior a 7.
-
-    return "";
+    return ( mean >= 7.0 ) ? "aprovado" : "reprovado";
 }
 
 /**
@@ -54,15 +76,27 @@ function isApproved(mean){
  * @param {*} strDate : Uma string no formato de data
  * @returns Uma descrição da data informada
  */
-function wide(strDate){
+ function wide(strDate){
 
     // TODO
     // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
     // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
     // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
     // Nesse caso, o separador é a barra (/) da data.
+    let strFullDate = "";
 
-    return "";
+    if ( strDate !== "" && ( ( /^(\d{2})\/(\d{2})\/(\d{4})$/.exec(strDate) ) 
+                        || ( /^(\d{2})\/(\d{2})\/(\d{2})$/.exec(strDate) ) ) ) {
+        const monthNamesPT_BR = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho",
+                                 "agosto", "setembro", "outubro", "novembro", "dezembro"];
+        
+        let arrDate = strDate.split("/");
+        let strDay = arrDate[0];
+        let intMonth = arrDate[1];
+        let strYear = arrDate[2];
+        strFullDate = strDay + " de " + monthNamesPT_BR[intMonth-1] + " de " + strYear;
+    } 
+    return strFullDate; 
 }
 
 // Trecho utilizado pelos testes
